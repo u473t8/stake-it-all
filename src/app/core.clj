337aa7c89d133c847911be145d1893d4d3236440
Shundeev Egor <shundeevegor@gmail.com>
2022-10-23 21:54:57 +0300

    Setup: add basic session management.

--- src/app/core.clj
+++ src/app/core.clj
@@ -4,40 +4,54 @@
     [aleph.netty :as netty]
     [muuntaja.core :as m]
     [reitit.ring :as ring]
-    [reitit.ring.middleware.muuntaja :as muuntaja]))
+    [reitit.ring.middleware.muuntaja :as muuntaja]
+    [ring.middleware.session :as ring-session]
+    [ring.middleware.session.memory :as memory]))
 
 
 (defonce servers (atom {}))
+(defonce session-store (memory/memory-store))
 
 
-(defn hello-page
-  [request]
-  {:status 200
-   :headers {"content-type" "text/plain"}
-   :body "Hello!\nWe are nice to meet you!"})
+(defn hello
+  [{:keys [session]}]
+  (if (seq session)
+    {:status 200
+     :body (format "Hi, %s! How do you do?" (:user session))}
+    {:status 200
+     :body (format "Hello! Who are you?")}))
 
 
 (defn greeting-message
   [request]
   {:status 200
-   :headers {"content-type" "application/json"}
    :body {:message (format "Hello, %s!" (:first-name (:body-params request)))}})
 
 
-(defn primitive-middleware [handler id]
-  (fn [request]
-    (handler (update request ::acc (fnil conj []) id))))
+(defn login
+  [request]
+  (let [user (:user (:body-params request))]
+    (if (contains? #{"egor" "petr"} user)
+      {:status 200
+       :session {:date (java.util.Date.)
+                 :user user}}
+      {:status 401})))
 
 
 (def app
   (ring/ring-handler
     (ring/router
       ["/"
-       ["" {:get  {:handler #'hello-page}
-            :post {:handler #'greeting-message}}]]
+       [[""
+         {:get  {:handler #'hello}
+          :post {:handler #'greeting-message}}]
+        ["login"
+         {:post {:handler #'login}}]]]
       {:data
        {:muuntaja m/instance
-        :middleware [muuntaja/format-middleware]}})))
+        :middleware [muuntaja/format-middleware
+                     [ring-session/wrap-session {:store session-store
+                                                 :cookie-attrs {:http-only false}}]]}})))
 
 
 (defn get-config
@@ -71,8 +85,13 @@
       (start-server! opts))))
 
 
+
 (comment
   @servers
-  (start-server! :port 5000)
-  (restart-server! :port 4000)
-  (stop-server! :port 5000))
+  (restart-server! :port 3434)
+  (stop-server! :port 3434)
+
+  (require '[ring.middleware.session.store :as store])
+
+  (store/delete-session session-store "57c4b788-f21c-4b9b-9595-077a4e09ac4b")
+  (store/read-session session-store "57c4b788-f21c-4b9b-9595-077a4e09ac4b"))
