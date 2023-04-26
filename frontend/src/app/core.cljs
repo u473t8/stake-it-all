c9c2536c2ba2525cc96d720b291e51be0827f940
Petr Maslov <petrmaslov@petrmaslov.ru>
2023-04-26 23:41:03 +0300

    Setup: add frontend sample.

--- frontend/src/app/core.cljs
+++ frontend/src/app/core.cljs
@@ -1,1 +1,17 @@
-(ns app.core)
+(ns app.core
+  (:require
+    [reagent.core :as r]
+    [reagent.dom :as dom]))
+
+
+(defn app
+  []
+  [:div [:h1 "Hi!"]])
+
+
+(defn mount-app []
+  (dom/render [app]
+    (.-body js/document)))
+
+
+(mount-app)
